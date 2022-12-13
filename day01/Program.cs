e02a01af738e654c562da87ce491197bd72cf16c
Derrick Eriksson <DERIKS23@volvocars.com>
2022-12-13 12:49:43 +0100

    Remove the read key line...

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,6 +1,5 @@
 ﻿var input = File.ReadAllText("input.txt");
 Console.WriteLine(Environment.GetEnvironmentVariable("part") is "part1" ? Part1(input) : Part2(input));
-Console.ReadKey();
 
 int Part1(string input) => input.Split("\r\n\r\n").Select(s => s.Split("\r\n").Sum(int.Parse)).Max();
 
