aa8aed4b3ce019c1a9eeac040b686f5ad7293c41
Derrick Eriksson <DERIKS23@volvocars.com>
2022-12-13 12:30:37 +0100

    Day 1 solution

---
+++ day01/Program.cs
@@ -0,0 +1,15 @@
+﻿var input = File.ReadAllText("input.txt");
+Console.WriteLine(Environment.GetEnvironmentVariable("part") is "part1" ? Solve.part1(input) : Solve.part2(input));
+
+public static class Solve
+{
+    public static int part1(string input)
+    {
+        return input.Split("\r\n\r\n").Select(s => s.Split("\r\n").Sum(x => int.Parse(x))).Max();
+    }
+
+    public static int part2(string input)
+    {
+        return input.Split("\r\n\r\n").Select(s => s.Split("\r\n").Sum(x => int.Parse(x))).OrderByDescending(x => x).Take(3).Sum(x => x);
+    }
+}
