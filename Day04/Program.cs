d0406b90f95f890109eedd6740e3ac91e6e471ac
Derrick Eriksson <DERIKS23@volvocars.com>
2022-12-14 11:59:31 +0100

    Refactor day 4 file, minor changes

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,22 +1,17 @@
-﻿using System.Runtime.CompilerServices;
-
-var input = File.ReadAllLines("input.txt").ToList();
+﻿var input = File.ReadAllLines("input.txt").ToList();
 
 Console.WriteLine(Environment.GetEnvironmentVariable("part") is "part1" ? Part1(input) : Part2(input));
-Console.ReadKey();
 
 int Part1(IEnumerable<string> input) => input.Select(i =>
 {
     var groups = i.Split(",").Select(Expand).ToList();
-    var result = groups[0].All(c => groups[1].Contains(c)) ? 1 : groups[1].All(c => groups[0].Contains(c)) ? 1 : 0;
-    return result;
+    return groups[0].All(c => groups[1].Contains(c)) ? 1 : groups[1].All(c => groups[0].Contains(c)) ? 1 : 0;
 }).Sum();
 
 int Part2(IEnumerable<string> input) => input.Select(i =>
 {
     var groups = i.Split(",").Select(Expand).ToList();
-    var result = groups[0].Any(c => groups[1].Contains(c)) ? 1 : 0;
-    return result;
+    return groups[0].Any(c => groups[1].Contains(c)) ? 1 : 0;
 }).Sum();
 
 List<int> Expand(string area)
