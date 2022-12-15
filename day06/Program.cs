2708f514b8922fa9c4f8922987bc07d073414df7
Derrick Eriksson <DERIKS23@volvocars.com>
2022-12-15 09:50:06 +0100

    Solution day 6

---
+++ day06/Program.cs
@@ -0,0 +1,25 @@
+﻿var input = File.ReadAllText("input.txt");
+
+Console.WriteLine(Environment.GetEnvironmentVariable("part") is "part1" ? Part1() : Part2());
+
+int Part1() => FindMarker(4);
+
+int Part2() => FindMarker(14);
+
+int FindMarker(int length)
+{
+    var sequenceFound = false;
+    var start = 0;
+
+    do
+    {
+        var s = input.Substring(start, length);
+        var uniqueLetters = s.Select(c => c).Distinct();
+        if (s.Select(c => c).Distinct().Count() == length)
+            sequenceFound = true;
+
+        start++;
+    } while (!sequenceFound);
+
+    return start + length - 1;
+}
